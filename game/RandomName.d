bb47f4dda474b3e5fd4cebc5cd07f1c2855b31a7
SiegeLord <slabode@aim.com>
2011-12-19 02:19:13 -0500

    Limit the star system names to 8 letters

--- game/RandomName.d
+++ game/RandomName.d
@@ -4,44 +4,50 @@ import tango.math.random.Random;
 
 char[] GenerateRandomName(Random random)
 {
-	const(char)[] consonants = "BBBCCCCCCDDDDDDFFFFFGGGGHHJKLLLLLLMMMMMMMNNNNNNNPPPPQRRRRSSSSSSTTTTTTTTTVVXZWY";
-	const(char)[] vowels = "AAAEEEEIIIOOOU";
-	const(char)[][] clusters = ["CR", "CH", "ZH", "TH", "SS", "TT", "RR", "WR", "TR", "NDR", "NT"];
-	
-	char get_vowel()
-	{
-		return vowels[random.uniformR(vowels.length)];
-	}
-	
-	char get_consonant()
-	{
-		return consonants[random.uniformR(consonants.length)];
-	}
-	
-	const(char)[] get_cluster()
-	{
-		return clusters[random.uniformR(clusters.length)];
-	}
-	
 	char[] ret;
-	
-	if(random.uniformR(2) == 0)
-		ret ~= get_vowel;
-	
-	size_t num_syllables = random.uniformR2(2, 3);
-	foreach(ii; 0..num_syllables)
+	while(true)
 	{
-		if(random.uniformR(10) == 0)
-			ret ~= get_cluster;
-		else
-			ret ~= get_consonant;
-		ret ~= get_vowel;
-		if(random.uniformR(4) == 0)
+		const(char)[] consonants = "BBBCCCCCCDDDDDDFFFFFGGGGHHJKLLLLLLMMMMMMMNNNNNNNPPPPQRRRRSSSSSSTTTTTTTTTVVXZWY";
+		const(char)[] vowels = "AAAEEEEIIIOOOU";
+		const(char)[][] clusters = ["CR", "CH", "ZH", "TH", "SS", "TT", "RR", "WR", "TR", "NDR", "NT"];
+		
+		char get_vowel()
+		{
+			return vowels[random.uniformR(vowels.length)];
+		}
+		
+		char get_consonant()
+		{
+			return consonants[random.uniformR(consonants.length)];
+		}
+		
+		const(char)[] get_cluster()
+		{
+			return clusters[random.uniformR(clusters.length)];
+		}
+		
+		if(random.uniformR(2) == 0)
 			ret ~= get_vowel;
+		
+		size_t num_syllables = random.uniformR2(2, 3);
+		foreach(ii; 0..num_syllables)
+		{
+			if(random.uniformR(10) == 0)
+				ret ~= get_cluster;
+			else
+				ret ~= get_consonant;
+			ret ~= get_vowel;
+			if(random.uniformR(4) == 0)
+				ret ~= get_vowel;
+		}
+		
+		if(random.uniformR(2) == 0)
+			ret ~= get_consonant;
+		
+		if(ret.length > 8)
+			ret.length = 0;
+		else
+			return ret;
 	}
-	
-	if(random.uniformR(2) == 0)
-		ret ~= get_consonant;
-	
-	return ret;
+	assert(0);
 }
